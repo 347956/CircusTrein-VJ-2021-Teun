28eb6d1812ca98475355974a4536a7d8819a8c8a
Teun <347956@student.fontys.nl>
2021-03-03 11:38:20 +0100

    Functionaliteit toegevoegd aan de buttons

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -20,32 +20,38 @@ namespace WindowsFormsApp1
 
         private void HerbivorL_Click(object sender, EventArgs e)
         {
-
+            train.AddAnimalsToList(Diet.Herbivor, Weight.Large);
+            lbAnimalsToBeAdded.Items.Add($"{Diet.Herbivor} | {Weight.Large}");
         }
 
         private void btnHerbivorM_Click(object sender, EventArgs e)
         {
-
+            train.AddAnimalsToList(Diet.Herbivor, Weight.Medium);
+            lbAnimalsToBeAdded.Items.Add($"{Diet.Herbivor} | {Weight.Medium}");
         }
 
         private void btnHerbivorS_Click(object sender, EventArgs e)
         {
-
+            train.AddAnimalsToList(Diet.Herbivor, Weight.Small);
+            lbAnimalsToBeAdded.Items.Add($"{Diet.Herbivor} | {Weight.Small}");
         }
 
         private void btnCarnivorL_Click(object sender, EventArgs e)
         {
-
+            train.AddAnimalsToList(Diet.Carnivor, Weight.Large);
+            lbAnimalsToBeAdded.Items.Add($"{Diet.Carnivor} | {Weight.Large}");
         }
 
         private void btnCarnivorM_Click(object sender, EventArgs e)
         {
-
+            train.AddAnimalsToList(Diet.Carnivor, Weight.Medium);
+            lbAnimalsToBeAdded.Items.Add($"{Diet.Carnivor} | {Weight.Medium}");
         }
 
         private void btnCarnivorS_Click(object sender, EventArgs e)
         {
-
+            train.AddAnimalsToList(Diet.Carnivor, Weight.Small);
+            lbAnimalsToBeAdded.Items.Add($"{Diet.Carnivor} | {Weight.Small}");
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -55,7 +61,8 @@ namespace WindowsFormsApp1
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            lbAnimalsToBeAdded.Items.Clear();
+            train.FillTrain();
         }
     }
 }
